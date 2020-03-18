d20f7ea216dff6b0f1b1a29e9222be06fa0ffe15
Tim Mirus <tim.mirus@stud.uni-regensburg.de>
2020-03-18 11:18:46 +0100

    simplify boxplot function

--- R/create_boxplots.R
+++ R/create_boxplots.R
@@ -3,43 +3,35 @@ create_boxplots <- function(results.df, metric = "cor") {
     overall.df <- results.df[which(results.df$cell_type == "overall"), ]
     # order algorithms by performance
     performances <- tapply(overall.df$score, overall.df$algorithm, median)
-    overall.df$algorithm <- factor(overall.df$algorithm, levels = levels(overall.df$algorithm)[order(performances)])
     results.df$algorithm <- factor(results.df$algorithm, levels = levels(overall.df$algorithm)[order(performances)])
     
-    metric <- overall.df$metric[1]
-    # plot results in boxplot
-    overall.plot <- ggplot(overall.df, aes(x = algorithm, y = score)) +
-    geom_boxplot(aes(col = algorithm)) +
-    ylim(0, 1) +
-    xlab("algorithm") +
-    ylab(paste("average overall", metric)) +
-    ggtitle("quality of deconvolution results",
-        subtitle = "varying number of training samples"
-    ) +
-    theme(
-        axis.text.x = element_text(size = 14),
-        axis.text.y = element_text(size = 16),
-        title = element_text(size = 22),
-        legend.text = element_text(size = 18),
-        legend.title = element_text(size = 20),
-        axis.title.x = element_text(size = 18),
-        axis.title.y = element_text(size = 18)
-    ) + 
-    scale_x_discrete(limits = levels(overall.df$algorithm))
+    metric <- results.df$metric[1]
 
+    # create boxplots for each cell type (including overall)
     cell.type.plots <- list()
     for(t in unique(results.df$cell_type)) {
-        if(t == "overall") next
         sub.df <- results.df[which(results.df$cell_type == t), ]
-	sub.df$algorithm <- factor(sub.df$algorithm, levels = levels(overall.df$algorithm))
         cell.type.plots[[t]] <- ggplot(sub.df, aes(x = algorithm, y = score)) +
             geom_boxplot(aes(col = algorithm)) +
             xlab("algorithm") +
             ylab(metric) +
-            ggtitle("quality of deconvolution results", subtitle = t)
+            ggtitle("quality of deconvolution results", subtitle = t)+
+            theme(
+                axis.text.x = element_text(size = 14),
+                axis.text.y = element_text(size = 16),
+                title = element_text(size = 22),
+                legend.text = element_text(size = 18),
+                legend.title = element_text(size = 20),
+                axis.title.x = element_text(size = 18),
+                axis.title.y = element_text(size = 18)
+            ) + 
+            scale_x_discrete(limits = levels(results.df$algorithm))
         if(metric == "cor"){
             cell.type.plots[[t]] <- cell.type.plots[[t]] + ylim(0,1)
         }
+        if(t == "overall") {
+            cell.type.plots[[t]] <- cell.type.plots[[t]] + ylab(paste("average overall", metric))
+        }
     }
-    return(list(score.plot = overall.plot, cell.type.plots = cell.type.plots))
+    return(list(cell.type.plots = cell.type.plots))
 }
