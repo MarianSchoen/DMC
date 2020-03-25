cacb1b0e4377411cede06b86580c53f380f1c8be
Marian Schön <marian.schoen@klinik.uni-regensburg.de>
2020-03-25 15:39:24 +0100

    roxygen skeleton update (check_algorithms.R)

--- R/check_algorithms.R
+++ R/check_algorithms.R
@@ -1,5 +1,12 @@
+#' check_algorithms
+#'
+#' @param algorithms list containing a list for each algorithm. Each sublist
+#' contains 1) name  and 2) function 
+#' @return
+#' @export
 check_algorithms <- function(algorithms){
-    randomRNA <- DTD::generate_random_data(
+    # generate random scRNA-seq like data 
+    random.data <- DTD::generate_random_data(
         n.types = 3,
         n.samples.per.type = 20,
         n.features = 25,
@@ -7,34 +14,42 @@ check_algorithms <- function(algorithms){
         feature.type = "gene",
         seed = 1234
     )
-
-    pheno.data <- sapply(strsplit(colnames(randomRNA), ".", fixed = TRUE),
+    # for each scRNA-seq profile, get its's "cell type"
+    pheno.data <- sapply(strsplit(colnames(random.data), ".", fixed = TRUE),
     FUN = function(x) {
         x[[2]]
     }
     )
-    names(pheno.data) <- colnames(randomRNA)
+    names(pheno.data) <- colnames(random.data)
+    # cast the cell type information into a data.frame
     pheno <- data.frame(sample.name = names(pheno.data), cell_type = pheno.data)
     rownames(pheno) <- pheno$sample.name
 
     # create bulks from random data
     bulks <- DTD::mix_samples(
-        expr.data = randomRNA,
+        expr.data = random.data,
         pheno = pheno.data,
         included.in.X = unique(pheno.data),
         n.samples = 20,
         n.per.mixture = 4
     )
 
+    # loop over all algorithms, run them on the random data, 
+    # and check wheter the output is valid
     for(a in algorithms){
         res <- a$algorithm(
-            randomRNA,
+            random.data,
             pheno,
             bulks$mixtures
         )
     	
         if(!is.list(res) || !all(c("est.props", "sig.matrix") %in% names(res))){
-                stop("Algorithm did not return expected values. Please check implementation")
+            stop(
+                paste0(
+                    "Algorithm ", a$name, " did not return expected values. 
+                    Please check implementation"
+                    )
+            )
         }
 	
     }
