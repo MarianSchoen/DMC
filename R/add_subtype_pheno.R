37ecb9f2bdc3892d6241f5f64f015ed02e3e1c25
Marian Schön <marian.schoen@klinik.uni-regensburg.de>
2020-06-24 14:42:08 +0200

    documentation for subtype stuff

--- R/add_subtype_pheno.R
+++ R/add_subtype_pheno.R
@@ -1,14 +1,30 @@
-#' add subtype pheno 
+#' add subtype pheno
 #'
-#' @param sc.counts 
-#' @param sc.pheno 
-#' @param cell.type.column 
-#' @param sample.name.column 
-#' @param new.subtype.column 
-#' @param hclust.obj 
-#' @param avg.profiles.per.subcluster.vec 
-#' @param verbose 
-#' @param ... 
+#' Use a hierarchical clustering of the input labels, 
+#' to generate finer subtype labels. 
+#' The basic steps are: 
+#'    1. use the get_hclust function to hierarchically cluster the data
+#'     within the cell types. (or use the provided hclust.obj)
+#'    2. cluster the data multiple times, with different depth 
+#'    (as indicated by 'avg.profiles.per.subcluster.vec')
+#'    3. provide the clustering labels as columns in sc.pheno, and return it.
+#'
+#' @param sc.counts count matrix, features as rows, scRNA-Seq profiles as columns
+#' @param sc.pheno data.frame. scRNA-Seq profiles as rows. Must have 
+#' 'cell.type.column' and 'sample.name.column' 
+#' @param cell.type.column string, column of 'sc.pheno' holding the 
+#' input cell type labels. Within these entries, the clustering is done. 
+#' @param sample.name.column string, column of the 'colnames(sc.counts)'
+#' @param new.subtype.column string, pattern of the new column, where the subtype
+#' information is stored
+#' @param hclust.obj 'hclust' object, or NA. If you already calculated a hclust, 
+#' object pass it here. Alternatively, we call the get_hclust function. 
+#' @param avg.profiles.per.subcluster.vec numeric vector, indicates how many new 
+#' subtype pheno entries are generated 
+#' (=> 'length(avg.profiles.per.subcluster.vec)') and - in average - how many
+#' profiles are in a subtype.
+#' @param verbose logical, should information about the process be printed. 
+#' @param ... arguments that are passed to get_hclust
 #'
 #' @return
 #'
