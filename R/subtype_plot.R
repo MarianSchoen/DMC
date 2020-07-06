7dc19227f613f22526d1b3315df2724bca889793
Tim Mirus <tim.mirus@stud.uni-regnsburg.de>
2020-07-06 15:16:01 +0200

    add subtype more plots

--- R/subtype_plot.R
+++ R/subtype_plot.R
@@ -1,9 +1,17 @@
 subtype_plot <- function(results.df){
   
   results.df <- results.df[which(results.df$cell_type == "overall"),]
-  p <- ggplot(results.df, aes(x = cluster_size, y = score, col = algorithm)) + 
+  overall.plot <- ggplot(results.df, aes(x = cluster_size, y = score, col = algorithm)) + 
     geom_line(aes(linetype = coarse)) +
     scale_x_reverse()
-  
-  return(p)
+  algorithm.plot <- ggplot(results.df, aes(x = cluster_size, y = score, col = algorithm)) + 
+    geom_line(aes(linetype = coarse)) +
+    scale_x_reverse() +
+    facet_grid(rows = vars(algorithm))
+  coarse.plot <- ggplot(results.df, aes(x = cluster_size, y = score, col = algorithm)) +
+    scale_x_reverse() +
+    geom_line() +
+    facet_grid(cols = vars(coarse))
+
+  return(list(overall = overall.plot, algorithm = algorithm.plot, coarse = coarse.plot))
 }
